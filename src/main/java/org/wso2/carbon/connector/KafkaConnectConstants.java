fc0f6ad6a17bff0e7b53027f5ff25ac03977e23a
Sajinie <sajiniekavindya@gmail.com>
2023-08-31 14:17:34 +0530

    Support configuring a header-prefix when filtering out the Kafka headers from message context

--- src/main/java/org/wso2/carbon/connector/KafkaConnectConstants.java
+++ src/main/java/org/wso2/carbon/connector/KafkaConnectConstants.java
@@ -83,7 +83,8 @@ public class KafkaConnectConstants {
     public static final String PARTITION_NO = "partitionNo";
     public static final String PARAM_KEY = "key";
 
-    public static final String METHOD_NAME = "publishMessages:";
+    public static final String KAFKA_HEADER_PREFIX = "kafka.kafkaHeaderPrefix";
+    public static final String DEFAULT_KAFKA_HEADER_PREFIX = "kafkaHeader_";
 
     // Configuration parameters for kafka connector
     public static final String KAFKA_BROKER_LIST = "kafka.bootstrapServers";
